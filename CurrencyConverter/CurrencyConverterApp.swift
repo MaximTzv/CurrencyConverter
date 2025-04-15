d2d6148da53d8b6284b0cba851b231afbad182f0
Maxim Tzvetkov <tzvetkovmaxim@gmail.com>
2025-04-15 19:26:32 +0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Maxim Tzvetkov on 15.04.25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
